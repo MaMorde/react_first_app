58820d0b393311f177c618a99344c7a81a5d660f
PoMorde <mamorde@yandex.ru>
2019-11-24 01:30:16 +0300

    1

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import s from './Content.module.css';
 
 const Content = () => {
   return (
-    <div className='app-content'>
-    <div>
-      <img src='https://images.unsplash.com/photo-1499084732479-de2c02d45fcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'></img>
-    </div>
-    <div>cc</div>
-    <div>
+    <div className={s.content}>
       <div>
-        cc
+        <img src='https://images.unsplash.com/photo-1499084732479-de2c02d45fcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80' alt=''></img>
+      </div>
+      <div>cc</div>
+      <div>
+        My posts
+        <div>
+          New posts
+        </div>
+      </div>
+      <div className={s.posts}>
+        <div className={s.item}>post 1</div>
+        <div className={s.item}>post 2</div>
       </div>
     </div>
-  </div>
+
   )
 }
 export default Content;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='app-header'>
-            <img className='header__logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/600px-DC_Comics_logo.png'></img>
+        <header className={s.header}>
+            <img className={s.logo} src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/600px-DC_Comics_logo.png' alt=''></img>
         </header>
     )
 }
