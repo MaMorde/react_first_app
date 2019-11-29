378bc532a5eb27c53af5c022aa457deeb1e35cc2
PoMorde <mamorde@yandex.ru>
2019-11-29 05:38:34 +0300

    update

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,27 +7,11 @@ import { NavLink } from "react-router-dom"
 
 const Dialogs = (props) => {
 
-    let dialogs = [
-        { id: 1, name: 'Emil' },
-        { id: 2, name: 'Tanya' },
-        { id: 3, name: 'Sonia' },
-        { id: 4, name: 'Sergei' },
-        { id: 5, name: 'Tolik' },
-        { id: 6, name: 'Artem' },
-    ]
 
-    let messages = [
-        { id: 1, message: 'hi' },
-        { id: 2, message: 'gf' },
-        { id: 3, message: 'gg' },
-        { id: 4, message: 'wg' },
-        { id: 5, message: 'ww' },
-        { id: 6, message: 'ww' },
-    ]
 
-    let dialogsElement = dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
+    let dialogsElement = props.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
 
-    let messagesElements = messages.map(m => <Message message={m.message} id={m.id} />)
+    let messagesElements = props.messages.map(m => <Message message={m.message} id={m.id} />)
 
     return (
         <div className={s.dialogs}>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,35 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+let posts = [
+    { id: 1, message: 'sssss', likesCount: 12 },
+    { id: 2, message: 'its my first post', likesCount: 11 },
+    { id: 3, message: 'its my first post', likesCount: 11 },
+    { id: 4, message: 'its my first post', likesCount: 11 },
+    { id: 5, message: 'its my first post', likesCount: 11 },
+]
+
+let dialogs = [
+    { id: 1, name: 'Emil' },
+    { id: 2, name: 'Tanya' },
+    { id: 3, name: 'Sonia' },
+    { id: 4, name: 'Sergei' },
+    { id: 5, name: 'Tolik' },
+    { id: 6, name: 'Artem' },
+]
+
+let messages = [
+    { id: 1, message: 'hi' },
+    { id: 2, message: 'gf' },
+    { id: 3, message: 'gg' },
+    { id: 4, message: 'wg' },
+    { id: 5, message: 'ww' },
+    { id: 6, message: 'ww' },
+]
+
+ReactDOM.render(<App posts={posts} dialogs={dialogs} messages={messages}  />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
